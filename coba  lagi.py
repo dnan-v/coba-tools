64deca04112e777eea6ba7ab2a14d3120563db7c
dnan-v <76686801+dnan-v@users.noreply.github.com>
2021-09-21 09:36:41 +0000

    xtacin-tools

---
+++ coba  lagi.py
@@ -0,0 +1,42 @@
+import time
+import socket
+import os
+import threading
+import numbers
+
+waktu = time.altzone
+socket_server = socket.os
+os_server = os._exit
+threading_server = threading._count
+numbers_START = numbers.Number
+
+print(waktu)
+print(socket_server)
+print(os_server)
+print(threading_server)
+print(numbers_START)
+
+print ("-------------------")
+print ("pembuatan : minggu-")
+print ("-- author : DNANJER")
+print ("--YOUTUBE : Xtacin ")
+
+pesan = ("DISCLAIMER TOOLS INI TAHAP PENGEMBANGAN KALO MASI BUG YA WAJAR OKE-")
+
+print (pesan)
+
+nomor = input("masukan nomor lu : ")
+sandi = input("berapa sandi lu : ")
+
+print(nomor)
+print(sandi)
+
+number = 0
+while number < 121:
+    a = input("memulai ketik run : ")
+    if a == "run":
+        for i in range(0,121,1):
+            print("hacking sedang berjalan......",i,"%")
+            if i == 91:
+                print("gagal melakukan hacking.....",i,"%")
+                print("error")
